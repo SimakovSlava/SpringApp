c659389dc03ff32268097c65569d1093b9a747a5
SimakovSlava <102996146+SimakovSlava@users.noreply.github.com>
2022-06-11 10:58:29 +0300

    SpringMvcAop
    second commit

---
+++ src/main/java/com/slavasimakov/spring/mvc_hibernate_aop/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.slavasimakov.spring.mvc_hibernate_aop.service;
+
+import com.slavasimakov.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
